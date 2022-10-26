fedff9ddcc6628656527c0f07512bed9c5cef952
psarge2 <paul@psarge.dev>
2022-10-26 15:21:15 -0400

    added actions to edit, update and delete a note, created and edited the appropriate views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-  before_action :set_note, only: %i[show]
+  before_action :set_note, only: %i[show edit update destroy]
   def index
     @notes = Note.all
   end
@@ -11,10 +11,45 @@ class NotesController < ApplicationController
     @note = Note.new
   end
 
+  def create
+    @note = Note.new(note_params)
+    respond_to do |format|
+      if @note.save
+        format.html {redirect_to note_url(@note), notice: "Note was created successfully"}
+      else
+        format.html {render :new, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html {redirect_to note_url(@note), notice: "Note was updated successfully"}
+      else
+        format.html {render :new, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @note.destroy
+    respond_to do |format|
+      format.html {redirect_to notes_url, notice: "Note was deleted sucessfully"}
+    end
+  end
+
 
   private
   
     def set_note
       @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :content)
     end
 end
